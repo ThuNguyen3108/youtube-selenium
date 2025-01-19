6017abaed2ca568dd8f3063fe82f08726ca1ccc0
ThuNguyen3108 <thuthudia123@gmail.com>
2025-01-19 18:38:54 +0700

    #comment

---
+++ src/main/java/buoi5/TinhDaHinh.java
@@ -0,0 +1,27 @@
+package buoi5;
+
+class Bike {
+    void run() {
+        System.out.println("Chạy bo");
+    }
+}
+public class TinhDaHinh extends Bike{
+    void run() {
+        System.out.println("Chay khoang 40km/gio");
+    }
+
+    void stop() {
+        System.out.println("Stop");
+    }
+
+    public static void main(String[] args) {
+        Bike bk = new Bike();
+        Bike bk1 = new TinhDaHinh();
+        TinhDaHinh bk2 = new TinhDaHinh();
+        bk.run();
+        bk1.run();
+        bk2.stop();
+    }
+    // thang con thi chi goi duoc con thoi
+    // thang cha khong the goi thang con
+}
